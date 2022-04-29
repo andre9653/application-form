1865b5c1a07800cbda90e067093bef80cd0c86fd
andre9653 <andresena9653@gmail.com>
2022-04-29 17:36:13 -0300

    feat: crea classe Result e um escopo da regra de negocio para criar objeto da avaliação

--- Back-End/src/routes.js
+++ Back-End/src/routes.js
@@ -2,4 +2,50 @@ const express = require('express');
 
 const routes = express.Router();
 
+class Result {
+  constructor() {
+    this.QuantidadePositiva = 0;
+    this.QuantidadeNegativa = 0;
+    this.QuantidadeNaoAvaliada = 0;
+  }
+
+  positive(num = 1) {
+    this.QuantidadePositiva += num;
+  }
+
+  negative() {
+    this.QuantidadeNegativa += 1;
+  }
+
+  notAvailed() {
+    this.QuantidadeNaoAvaliada += 1;
+  }
+}
+
+routes.post('/submit', (req, res) => {
+  const responses = { ...req.body };
+
+  const result = new Result();
+
+  Object.values(responses).forEach((response) => {
+    switch (response) {
+      case 'Sim':
+        result.positive();
+        break;
+      case 'Não':
+        result.negative();
+        break;
+      case 'Não Sei':
+        result.notAvailed();
+        break;
+      case 'Agora!!':
+        result.positive(2);
+        break;
+      default:
+        console.log('invalid');
+        break;
+    }
+  });
+});
+
 module.exports = routes;
