3d87ba2c72f00c63d5e8f1c74bb5cc36c11d16cf
andre9653 <andresena9653@gmail.com>
2022-04-29 17:02:51 -0300

    feat: primeiros treixos de codigo

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -1,12 +1,14 @@
 require('express-async-errors');
 const express = require('express');
 require('dotenv').config();
+const routes = require('./routes');
 
 const { PORT, HOST } = process.env;
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 const server = app.listen(PORT, HOST, () => {
   console.log(`Start server on PORT ${PORT}`);
