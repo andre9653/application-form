a6c5de7b0ee58df87055e272601fde340cdaac2a
André Sena <81939556+andre9653@users.noreply.github.com>
2022-05-01 17:18:18 -0300

    Revert "feat: primeiros treixos de codigo"

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -1,14 +1,12 @@
 require('express-async-errors');
 const express = require('express');
 require('dotenv').config();
-const routes = require('./routes');
 
 const { PORT, HOST } = process.env;
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 
 const server = app.listen(PORT, HOST, () => {
   console.log(`Start server on PORT ${PORT}`);
