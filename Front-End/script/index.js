4c5b03b59c4832cf463028df6ca03fe1ac3662ee
andre9653 <andresena9653@gmail.com>
2022-05-03 19:07:51 -0300

    feat: acrescenta estilo as validações e escopo da ação de enviar formulario

--- Front-End/script/index.js
+++ Front-End/script/index.js
@@ -28,10 +28,37 @@ function selectOption() {
     const len = textValue.length;
     count.innerText = len;
     if (len < 15) count.style.color = 'yellow';
-    if (len >= 15) count.style.color = 'blue';
+    if (len >= 15) count.style.color = '#edf0ff';
     if (len === 200) count.style.color = 'red';
     storage[keyForm] = textValue;
     localStorage.forms = JSON.stringify(storage, 2);
+  });
+}
+
+function handleSubmit() {
+  const button = document.querySelector('#submit');
+  button.addEventListener('click', async () => {
+    const storage = JSON.parse(localStorage.forms);
+    let ok = true;
+    Object.entries(storage).forEach((pergunta) => {
+      if (!pergunta[1]) {
+        ok = false;
+        document.querySelector(`.${pergunta[0]}`).classList.add('problem');
+      }
+    });
+    if (!ok) alert('os campos destacados não foram informados!');
+    else if (storage.pergunta4.length < 15) {
+      ok = false;
+      document.querySelector('.pergunta4').classList.add('problem');
+      alert('Sua resposta deve ter pelomenos 15 caracteres');
+    }
+
+    if (ok) {
+      const url = 'http://localhost:3000/submit';
+      const request = await fetch(url, { method: post, body: storage });
+      const response = await request.json();
+      console.log(response);
+    }
   });
 }
 
@@ -44,6 +71,8 @@ const formsObject = {
 };
 
 window.onload = () => {
-  if (!localStorage.forms) localStorage.setItem('forms', JSON.stringify(formsObject, 2));
+  localStorage.clear();
+  localStorage.setItem('forms', JSON.stringify(formsObject, 2));
   selectOption();
+  handleSubmit();
 };
